3f9b89025eae6caa12cdfec73793c904bcc4398f
mayaguhan <i.am.maya.guhan@gmail.com>
2021-11-16 13:46:21 +0800

    Updates to download pages
    Completed Download pages for China and Thailand.

--- src/pages/downloadsChina.jsx
+++ src/pages/downloadsChina.jsx
@@ -1,70 +1,114 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { Context } from '../components/store/Store';
-
 import Loading from '../components/loading/loading.component';
-import { LinearScale } from 'chart.js';
-
-
-
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 function DownloadChina(props) {
 
     const [loading, setLoading] = useState(true);
     const [state, dispatch] = useContext(Context);
-    const [commodityList, setCommodityList] = useState([]);
-    const [APILinks, setAPILinks] = useState({});
+    const [APILinks, setAPILinks] = useState([]);
+    const [saveNew, setSaveNew] = useState(false);
+    const [commodities, setCommodities] = useState([]);
 
+    const saveAllExcel = (event) => {
+        setLoading(true);
+        axios.get(`${state.api}/china/saveallexcel`)
+        .then(resp => {
+            console.log(resp);
+            if (resp.status == 200){
+                setSaveNew(true);
+                setLoading(false);
+            } else{
+                alert("There was an issue saving latest excel files.")
+                setLoading(false);
+            }
+        })
+    }
 
     useEffect(() => {
         axios.get(`${state.api}/china/commodities`)
         .then(res => {
-            res.data.sort();
-            setCommodityList(res.data);
-            var linksArray = {};
-            for (const commodity of res.data){
-                console.log(commodity);
-                let newCommodity = commodity.replace(/ /g, "+");
-                axios.get(`${state.api}/s3/retrieves3link/China/` + newCommodity)
-                .then(res => {
-                    // console.log(linksArray);
-                    linksArray[commodity] = res.data.url;
-                }, (error) => {
-                    console.log(error);
-                });
-            }
-            
-            console.log(linksArray);
-            setAPILinks(linksArray);
-            setLoading(false);
+            let comms = res.data;
+            comms.sort();
+            setCommodities(comms);
         }, (error) => {
-          console.log(error);
+            console.log(error);
         });
     },[])
 
     useEffect(() => {
-        console.log(commodityList);
+        let linksArray = Array(commodities.length).fill('');
+        commodities.forEach((commodity, idx) => {
+            let newCommodity = commodity.replace(/ /g, "+");
+            axios.get(`${state.api}/s3/retrieves3link/China/` + newCommodity)
+            .then(resp => {
+                linksArray[idx] = resp.data.url;
+                if (idx == commodities.length - 1) {
+                    setAPILinks(linksArray);
+                }
+            }).catch(error => {
+                console.log(error);
+            });
+        })
+    }, [commodities, saveNew])
+
+    useEffect(() => {
         console.log(APILinks);
-      }, [APILinks])
-  
+        setLoading(false);
+    }, [APILinks])
     return (
       <>
         {
             loading ?
-            (
                 <Loading />
-            )
-            :
-            (
-                Object.keys(APILinks).map(k => (
-                    <li
-                     key={k}
-                    ><strong>{k}</strong>: {APILinks[k]}</li>
-                    )
-                )
-            )
-            
+                :
+                <Box my={3} mx={1.5} style={{ textAlign: "center" }}>
+                    <Typography variant="h3" >
+                        China Downloads
+                    </Typography>
+                    <Button variant="contained" sx={{ ml: 1, mt : 2 }} onClick={saveAllExcel}>
+                            Save Latest Excel
+                    </Button>
+                    <TableContainer component={Paper} sx={{ width: "80%", margin: "auto", mt: 5,textAlign: "center" }}>
+                        <Table aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Commodity</TableCell>
+                                    <TableCell align="right">Link</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {APILinks.map((link, idx) => (
+                                    <TableRow
+                                    key={idx}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {commodities[idx]}
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <Button variant="contained" href={link}>
+                                                {link}
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Box>
         }
       </>
     )
