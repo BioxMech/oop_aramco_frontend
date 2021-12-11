e9f942e236b350e14f44d7cd314d66c69f15d97f
Jason <livelivelivelive12345@gmail.com>
2021-12-11 14:36:27 +0800

    Revert "Update Store.jsx"
    This reverts commit a1bd1e26fc29d098f9cc4e15be3206b4a12b3884.

--- src/components/store/Store.jsx
+++ src/components/store/Store.jsx
@@ -1,9 +1,8 @@
 import React, { createContext, useReducer } from 'react';
 
 const initialState = {
-    // api: "http://ec2-3-84-143-135.compute-1.amazonaws.com:8080/api"
+    api: "http://ec2-3-84-143-135.compute-1.amazonaws.com:8080/api"
     // api: "http://localhost:8080/api"
-    api: "https://aramco-backend.herokuapp.com/"
 }
 
 const Reducer = (state, action) => {
