125e15fe78c9dee21231e744c9a6735e0b510289
Jason <livelivelivelive12345@gmail.com>
2021-12-11 14:37:13 +0800

    Pointing to the correct URL for backend calls

--- src/components/store/Store.jsx
+++ src/components/store/Store.jsx
@@ -1,8 +1,9 @@
 import React, { createContext, useReducer } from 'react';
 
 const initialState = {
-    api: "http://ec2-3-84-143-135.compute-1.amazonaws.com:8080/api"
+    // api: "http://ec2-3-84-143-135.compute-1.amazonaws.com:8080/api"
     // api: "http://localhost:8080/api"
+    api: "https://aramco-backend.herokuapp.com/api"
 }
 
 const Reducer = (state, action) => {
